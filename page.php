20a6cc587aab87f52d197feaad019739a0b0957e
Calvin Chong <cvinss309ty@gmail.com>
2019-12-13 14:52:11 +0800

    Delete page.php

--- page.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-   <head>
-      <title>Testing page</title>
-   </head>
-   <body>
-      <h1 style='margin-top: 9vh;'><center>This is page.html</center></h1>
-   </body>
-</html>
